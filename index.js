eb32ad82746069d6def90442f07e9beb2cb1998c
Tolgahan Çelik <tol_celik@hotmail.com>
2024-02-13 23:12:03 +0300

    Parse condition updated.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function isArray(val) {
 }
 
 function parseValue(val) {
-  if (typeof val === 'undefined' || val === '') {
+  if (typeof val === 'undefined') {
     return null;
   } else if (isBoolean(val)) {
     return parseBoolean(val);
